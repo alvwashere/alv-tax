0a7cd2bfad0512095f0cdf11e050d0f3fb3b64fc
Alv <72032190+alvwashere@users.noreply.github.com>
2021-05-21 09:04:19 +0100

    Added Balance Checks and Notifications
    The script not uses xPlayer.getAccount('bank') to determine if the player that is being taxed by the government has enough funds to pay their taxes, this prevents them from going into -$ in-game as that has caused issues with some servers.
    
    I have now also added notification systems so if you pass the balance check, you are notified that your taxes are deducted from your balance set in Config.lua, and if you fail the check, the server gets a notification that law enforcement are looking for you, as you will be declared wanted for tax evasion.
    
    I have also beautified the code, and thank you to Lance Good for the idea (github.com/DevLanceGood) the tax system will soon run off of percentages, which will be set as a Config due to the percentage differentiating between countries; like the UK where VAT (TAX) = 20%! 😄

--- sv_tax.lua
+++ sv_tax.lua
@@ -1,18 +1,63 @@
 ESX = nil
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
+TriggerEvent(
+    "esx:getSharedObject",
+    function(obj)
+        ESX = obj
+    end
+)
+
+AddEventHandler(
+    "playerSpawned",
+    function()
+        TriggerEvent("alv_tax:chargePlayer")
+    end
+)
+
+ESX = nil
+
+TriggerEvent(
+    "esx:getSharedObject",
+    function(obj)
+        ESX = obj
+    end
+)
 
 RegisterNetEvent("alv_tax:chargePlayer")
-AddEventHandler("alv_tax:chargePlayer", function()
-    local xPlayer = ESX.GetPlayerFromId(source)
+AddEventHandler(
+    "alv_tax:chargePlayer",
+    function()
+        local xPlayer = ESX.GetPlayerFromId(source)
+        playerName = GetRealPlayerName(playerId)
 
-    if Config.Enabled then
-        Wait(Config.TimeInterval)
+        if Config.Enabled then
+            Wait(Config.TimeInterval)
 
-        xPlayer.removeAccountMoney(Config.Account, Config.TaxAmount)
+            balance = xPlayer.getAccount("bank").money
+            if balance > Config.TaxAmount then
+                xPlayer.removeAccountMoney(Config.Account, Config.TaxAmount)
+                ESX.ShowNotification(
+                    "Your taxes have been ~g~paid~s~ and ~b~deducted~s~ from your ~b~" .. Config.Account .. "~s~!"
+                )
+            else
+                if balance < Config.TaxAmount then
+                    ESX.ShowNotification("You are now wanted for ~r~Tax Evasion~s~!")
+                    TriggerClientEvent(
+                        "chatMessage",
+                        -1,
+                        "Government",
+                        {255, 0, 0},
+                        "Player" .. playerName .. "has been declared guilty of tax evasion! Law Enforcement Alerted!"
+                    )
+                end
+            end
+        end
     end
-end)
+)
 
-AddEventHandler('playerSpawned', function()
-    TriggerEvent("alv_tax:chargePlayer")
-end)
+AddEventHandler(
+    "playerSpawned",
+    function()
+        TriggerEvent("alv_tax:chargePlayer")
+    end
+)
