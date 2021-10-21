d4c66a6517f87d0e9f90f8db7d6352f23cf60217
Aspect <90366824+AsepctDEVS@users.noreply.github.com>
2021-10-21 11:08:05 -0500

    Fixed.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,6 +2,6 @@ fx_version 'adamant'
 game 'gta5'
 author 'alv'
 description 'tax sys - uses esx'
-
+url "https://github.com/AsepctDEVS"
 server_script 'sv_tax.lua'
-client_script 'config.lua'
+shared_script 'config.lua'
