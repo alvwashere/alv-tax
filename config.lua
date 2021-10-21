f4b19bee6c6dc28783afebf899f02c9fdeffe0dd
Aspect <90366824+AsepctDEVS@users.noreply.github.com>
2021-10-21 11:10:44 -0500

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,8 +1,8 @@
-Config = {}
+Tax = {}
 
-Config.UsePercentage = true -- If you have this enabled, Config.TaxAmount will not work! 
-Config.PercentageToCharge = 20 -- Make this from a value of 1-100, this will remove the percent amount from players chosen account
-Config.TaxAmount = 5000 -- How much the player should be taxed in the time interval if you are not using Config.UsePercentage!
-Config.TimeInterval = 12000000 -- The amount of time until player should be taxed, default is 20 minutes!
-Config.Account = 'bank' -- Make this bank or cash if ESX v1.1 - else bank or money (or of course black_money :) )
-Config.Enabled = true -- Use this to enable/disable the script!
+Tax.UsePercentage = true -- If you have this enabled, Tax.TaxAmount will not work! 
+Tax.PercentageToCharge = 20 -- Make this from a value of 1-100, this will remove the percent amount from players chosen account
+Tax.TaxAmount = 5000 -- How much the player should be taxed in the time interval if you are not using Tax.UsePercentage!
+Tax.TimeInterval = 12000000 -- The amount of time until player should be taxed, default is 20 minutes!
+Tax.Account = 'bank' -- Make this bank or cash if ESX v1.1 - else bank or money (or of course black_money :) )
+Tax.Enabled = true -- Use this to enable/disable the script!
