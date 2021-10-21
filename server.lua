8c5a7b3c3b9482306986a8d7c8d60081dd0ff83e
Aspect <90366824+AsepctDEVS@users.noreply.github.com>
2021-10-21 11:11:22 -0500

    Update server.lua

--- server.lua
+++ server.lua
@@ -15,30 +15,30 @@ AddEventHandler(
         local xPlayer = ESX.GetPlayerFromId(_source)
         playerName = GetRealPlayerName(_source)
 
-        if Config.Enabled then
-            Wait(Config.TimeInterval)
+        if Tax.Enabled then
+            Wait(Tax.TimeInterval)
 
-            if Config.UsePercentage then
-                local currentMoney = xPlayer.getAccount(Config.Account).money
-                local percent = 0.01 * Config.PercentageToCharge
+            if Tax.UsePercentage then
+                local currentMoney = xPlayer.getAccount(Tax.Account).money
+                local percent = 0.01 * Tax.PercentageToCharge
                 local amountToRemove = currentMoney * percent
 
-                xPlayer.removeAccountMoney(Config.Account, amountToRemove)
+                xPlayer.removeAccountMoney(Tax.Account, amountToRemove)
 
                 ESX.ShowNotification(
                     "Your taxes have been ~g~paid~s~, you have been charged" ..
-                        amountToRemove .. "from your" .. Config.Account .. "!"
+                        amountToRemove .. "from your" .. Tax.Account .. "!"
                 )
             end
         else
             balance = xPlayer.getAccount("bank").money
-            if balance > Config.TaxAmount then
-                xPlayer.removeAccountMoney(Config.Account, Config.TaxAmount)
+            if balance > Tax.TaxAmount then
+                xPlayer.removeAccountMoney(Tax.Account, Tax.TaxAmount)
                 ESX.ShowNotification(
-                    "Your taxes have been ~g~paid~s~ and ~b~deducted~s~ from your ~b~" .. Config.Account .. "~s~!"
+                    "Your taxes have been ~g~paid~s~ and ~b~deducted~s~ from your ~b~" .. Tax.Account .. "~s~!"
                 )
             else
-                if balance < Config.TaxAmount then
+                if balance < Tax.TaxAmount then
                     ESX.ShowNotification("You are now wanted for ~r~Tax Evasion~s~!")
                     TriggerClientEvent('chat:addMessage', -1, {
 		template = '<div style="padding: 0.5vw; margin: 0.5vw; background-color: rgba(128, 128 ,128, 1); border-radius: 3px;"><i class="fas fa-users"></i> <b>[Government!] ' .. playerName .. '</b> <i>has been declared guilty of tax evasion! Law Enforcement Alerted.</i></div>'
